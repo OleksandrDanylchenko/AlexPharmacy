a29f0084ae8cacfe7fa204dafc9cc4a143bfc084
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-11 20:46:09 +0300

    Added buttons on landing page

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -1,25 +1,60 @@
 <template>
   <div>
-    <div class="display-4 text-center">
-      Адміністративна панель<br />
-      аптечної мережі<br />
-      Alex Pharmacy
-    </div>
+    <div class="d-flex justify-content-center flex-wrap">
+      <div class="display-3 text-center mt-3 text-primary">
+        Адміністративна панель<br />
+        аптечної мережі<br />
+        Alex Pharmacy
+      </div>
 
-    <router-link
-      to="/cities"
-      tag="button"
-      class="btn btn-success rounded-pill w-100 my-2"
-    >
-      <h2 class="m-0">Міста-філіали</h2>
-    </router-link>
-    <router-link
-      to="/cities"
-      tag="button"
-      class="btn btn-success rounded-pill w-100 my-2"
-    >
-      <h2 class="m-0">Міста-філіали</h2>
-    </router-link>
+      <router-link
+        to="/cities"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 mt-4 mb-3"
+      >
+        <h2 class="m-0">Міста-філіали</h2>
+      </router-link>
+
+      <router-link
+        to="/manufacturers"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 my-3"
+      >
+        <h2 class="m-0">Виробники</h2>
+      </router-link>
+
+      <router-link
+        to="/clients"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 my-3"
+      >
+        <h2 class="m-0">Клієнти</h2>
+      </router-link>
+
+      <router-link
+        to="/drugs"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 my-3"
+      >
+        <h2 class="m-0">Препарати</h2>
+      </router-link>
+
+      <router-link
+        to="/diseases"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 my-3"
+      >
+        <h2 class="m-0">Хвороби</h2>
+      </router-link>
+
+      <router-link
+        to="/baskets"
+        tag="button"
+        class="btn btn-success rounded-pill w-75 my-3"
+      >
+        <h2 class="m-0">Кошики</h2>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -28,3 +63,7 @@ export default {
   name: "Landing"
 };
 </script>
+
+<style lang="css">
+@import "../styles/gradient.css";
+</style>
