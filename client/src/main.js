a0206116bec66e9039636d91c08c63a04e678b57
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-11 18:56:08 +0300

    Added all needed libraries

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes";
 
-Vue.config.productionTip = false
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+
+import VeeValidate, {
+  ValidationObserver,
+  ValidationProvider,
+  Validator
+} from "vee-validate";
+Vue.use(VeeValidate, { fieldsBagName: "veeFields", errorBagName: "veeErrors" });
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+
+import uk from "vee-validate/dist/locale/uk";
+Validator.localize("uk", uk);
+
+import vWow from "v-wow";
+Vue.use(vWow);
+
+Vue.config.productionTip = true;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  router,
+  render: h => h(App)
+}).$mount("#app");
