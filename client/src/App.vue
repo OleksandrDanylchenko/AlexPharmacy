d14eaeccce58372b5d600e1a649bd503cd3bae9f
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-12 18:54:31 +0300

    Added copyright

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,9 @@
       integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
       crossorigin="anonymous"
     />
+    <p id="copyright" class="text-primary">
+      Alexander Danylchenko, Databases Work 2, 2020
+    </p>
     <b-container>
       <router-view />
     </b-container>
@@ -17,3 +20,13 @@ export default {
   name: "App"
 };
 </script>
+
+<style>
+#copyright {
+  position: absolute;
+  top: 10px;
+  right: 0;
+  writing-mode: vertical-lr;
+  text-orientation: mixed;
+}
+</style>
