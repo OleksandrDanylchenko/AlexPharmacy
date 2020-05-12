000a45f33550188b53eee8c6c543c6328cc79308
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-12 21:41:17 +0300

    Converted copyright into href

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,9 +6,13 @@
       integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
       crossorigin="anonymous"
     />
-    <p id="copyright" class="text-primary">
+    <a
+      id="copyright"
+      class="text-primary"
+      href="https://github.com/OlexanderD/AlexPharmacy"
+    >
       Alexander Danylchenko, Databases Work 2, 2020
-    </p>
+    </a>
     <b-container>
       <router-view />
     </b-container>
@@ -25,8 +29,9 @@ export default {
 #copyright {
   position: absolute;
   top: 10px;
-  right: 0;
+  right: 10px;
   writing-mode: vertical-lr;
   text-orientation: mixed;
+  text-decoration: none;
 }
 </style>
