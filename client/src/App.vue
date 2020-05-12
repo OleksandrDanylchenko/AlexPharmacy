14ded66a22cd68ce9c085715744096c9bd02d4aa
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-13 02:27:47 +0300

    Moved copyright to separate file

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,13 +6,7 @@
       integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
       crossorigin="anonymous"
     />
-    <a
-      id="copyright"
-      class="text-primary"
-      href="https://github.com/OlexanderD/AlexPharmacy"
-    >
-      Alexander Danylchenko, Databases Work 2, 2020
-    </a>
+    <Copyright />
     <b-container>
       <router-view />
     </b-container>
@@ -20,18 +14,12 @@
 </template>
 
 <script>
+import Copyright from "./components/common/Copyright";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Copyright
+  }
 };
 </script>
-
-<style>
-#copyright {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  writing-mode: vertical-lr;
-  text-orientation: mixed;
-  text-decoration: none;
-}
-</style>
