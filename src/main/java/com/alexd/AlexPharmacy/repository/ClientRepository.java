9ac68ead25d73d8cfff659ffde4cabed7a1193c2
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-05-15 17:01:32 +0300

    Added proper first clients request

--- src/main/java/com/alexd/AlexPharmacy/repository/ClientRepository.java
+++ src/main/java/com/alexd/AlexPharmacy/repository/ClientRepository.java
@@ -13,11 +13,10 @@ import java.util.List;
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    @Query(value = "SELECT clients.last_name + ' ' + CONVERT(VARCHAR(15), clients.birthday, 4)"
-            + "FROM clients WHERE clients.id IN "
+    @Query(value = "SELECT * FROM clients WHERE clients.id IN "
             + "( SELECT baskets.client_id FROM baskets WHERE baskets.drug_id IN "
             + "( SELECT drugs.id FROM drugs WHERE drugs.manufacturer_id IN "
             + "( SELECT manufacturer_id FROM manufacturers WHERE manufacturers.trademark = ?1 ) ) )",
             nativeQuery = true)
-    List<String> findLastNameAndBirthdayClientWhoBoughtDrugByManufacturer(String manufacturerName);
+    List<Client> findLastNameAndBirthdayClientWhoBoughtDrugByManufacturer(String manufacturerName);
 }
